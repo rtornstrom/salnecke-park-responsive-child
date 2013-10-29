2530422e3d6e376a40698561871f0059577d934e
Roger Tornstrom <roger@nilrogsplace.se>
2013-10-29 22:25:59 +0100

    Added page content before the photo loop.

--- blog-photos.php
+++ blog-photos.php
@@ -24,9 +24,44 @@ global $more; $more = 0;
 ?>
 
 <div id="content-blog" class="<?php echo implode( ' ', responsive_get_content_classes() ); ?>">
-
-	<?php get_template_part( 'loop-header' ); ?>
+	
+	<?php if( have_posts() ) : ?>
+		
+		<?php while( have_posts() ) : the_post(); ?>
 			
+			<?php get_template_part( 'loop-header' ); ?>
+			
+			<?php responsive_entry_before(); ?>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				
+				<?php responsive_entry_top(); ?>
+				
+				<?php get_template_part( 'post-meta-page' ); ?>
+				
+				<div class="post-entry">
+					<?php the_content() ?>
+					
+				</div>
+				
+				<?php get_template_part( 'post-data' ); ?>
+				
+				<?php responsive_entry_bottom(); ?>
+			
+			</div><!-- end of #post-<?php the_ID(); ?> -->
+			<?php responsive_entry_after(); ?>
+			
+	<?php
+		endwhile;
+
+		get_template_part( 'loop-nav' );
+
+	else :
+
+		get_template_part( 'loop-no-posts' );
+
+	endif;
+	?>
+
 	<?php 
 	global $wp_query, $paged;
 	if( get_query_var( 'paged' ) ) {
